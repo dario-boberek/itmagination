40296e1778df8a988f0d3e3b41aba29fc0821665
Dariusz Boberek <d.boberek@gmail.com>
2018-09-28 17:43:14 +0200

    added gitignore and some decription what to do - switching to itm2 for now

--- heyheroku/src/test/java/com/iteration21/itmagination/AppTest.java
+++ heyheroku/src/test/java/com/iteration21/itmagination/AppTest.java
@@ -11,12 +11,37 @@ public class AppTest
     @Test
     public void daneDoPolaczeniaSie()
     {
+        //i21c HEROKU HELLO
+        // https://devcenter.heroku.com/articles/getting-started-with-java
+        //switching to C:\meta\data\git\itm2 for now
+
+
+
         //i21c CREDENTIALS
         // github.com d.boberek@gmail.com:i#
         // repository: https://github.com/iteration21/itmagination
         // heroku.com d.boberek@gmail.com:i#
         // ssh keys https://github.com/settings/keys -> earth public, on tableteartch privae $ cd ~/.ssh/
         // id_rsa       id_rsa.pub   known_hosts
+
+        //i21c LOCAL GIT SETUP
+        // integrate with github:
+        // ls -al ~/.ssh
+        // eval $(ssh-agent -s)
+        // ssh-add ~/.ssh/id_rsa
+        // clip < ~/.ssh/id_rsa.pub
+        // cd /c/meta/data/git
+        // git clone git@github.com:iteration21/itmagination.git itm
+        // cd itm
+        // ls
+
+        //additional 1st time git setup:
+        //git config --global user.name "Dariusz Boberek"
+        //git config --global user.email d.boberek@gmail.com
+        //git config --global core.editor "'C:/meta/programs/notepad++/notepad++.exe' -multiInst -nosession"
+        //git config --list
+        //s: https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup
+
         assertTrue( true );
     }
 }
